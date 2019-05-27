e3bd379089622d2ce8a02fb2682737f0c36ada2a
Daniel Bos <corani@gmail.com>
2019-05-27 22:26:50 +0800

    more config options

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
@@ -11,18 +12,23 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/fatih/color"
+	"github.com/mitchellh/go-homedir"
 )
 
 /*
 TODO(daniel) Don't use `log` for output, just `fmt.Printf` instead.
 */
 type Config struct {
-	Patterns []string `json:"patterns"`
-	Assignee string   `json:"assignee"`
-	Include  []string `json:"include"`
-	Exclude  []string `json:"exclude"`
-	Format   string   `json:"format"`
-	Output   string   `json:"output"`
+	Patterns          []string `json:"patterns"`
+	Assignee          string   `json:"assignee"`
+	IncludeUnassigned bool     `json:"includeUnassigned"`
+	Include           []string `json:"include"`
+	Exclude           []string `json:"exclude"`
+	Format            string   `json:"format"`
+	Output            string   `json:"output"`
+	RelRoot           string   `json:"relRoot"`
 }
 
 func main() {
@@ -45,18 +51,34 @@ func main() {
 		log.Fatalf("unable to parse config file '%s': %v", *configFile, err)
 	}
 
-	// TODO(daniel): Use include/exclude patterns here. Since the Go standard library doesn't support
-	// double-star globs, we need to write our own matcher here.
 	var paths []string
-	filepath.Walk("/home/dbos/go/src", func(path string, info os.FileInfo, err error) error {
+
+	for _, path := range config.Include {
+		path, err := homedir.Expand(path)
 		if err != nil {
-			return err
+			log.Fatalf("unable to expand homedir: %v", err)
 		}
-		if info.Mode().IsRegular() && strings.HasSuffix(info.Name(), ".go") {
-			paths = append(paths, path)
+
+		// TODO(daniel): Use include/exclude patterns here. Since the Go standard library doesn't support
+		// double-star globs, we need to write our own matcher here.
+		filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.Mode().IsRegular() && strings.HasSuffix(info.Name(), ".go") {
+				paths = append(paths, path)
+			}
+			return nil
+		})
+	}
+
+	var root string
+	if config.RelRoot != "" {
+		root, err = homedir.Expand(config.RelRoot)
+		if err != nil {
+			log.Fatalf("unable to expand homedir: %v", err)
 		}
-		return nil
-	})
+	}
 
 	for _, path := range paths {
 		fs := token.NewFileSet()
@@ -66,27 +88,54 @@ func main() {
 		}
 
 		for _, cg := range ast.Comments {
-			found := false
+			found := ""
+			first := true
 			comments := getCommentLines(fs, cg)
+		comment:
 			for _, c := range comments {
-				// TODO(daniel): Support having more than one match within the same comment group
-				if !found {
-					for _, pattern := range config.Patterns {
-						// TODO(daniel): Extract assignee
-						if pos := strings.Index(c.Text, pattern); pos == 0 {
-							// TODO(daniel): Increment the c.Slash position, so we land exactly on the pattern (skip e.g. "// ")
-							found = true
+				for _, pattern := range config.Patterns {
+					if pos := strings.Index(c.Text, pattern); pos == 0 {
+						// NOTE(daniel) If an Assignee has been set, skip all comments that have a different
+						// assignee (but include ones without an assignee, if requested).
+						if !strings.HasPrefix(c.Text, pattern+"(") {
+							if !config.IncludeUnassigned {
+								continue comment
+							}
+						} else if config.Assignee != "" {
+							if !strings.HasPrefix(c.Text, fmt.Sprintf("%s(%s)", pattern, config.Assignee)) {
+								continue comment
+							}
 						}
+						found = pattern
+						first = true
 					}
 				}
 				// TODO(daniel): Is there some way to get the context of the comment group? It would be nice if
 				// we could print a few code-lines, as the comments might not always make sense otherwise
 				// TODO(daniel): Do we need some kind of intermediate representation, before we output?
-				// TODO(daniel): Only report the position for the first line (containing the pattern), not for the following lines
-				// TODO(daniel): Truncate the path prefix (perhaps based on a config value) or make it relative?
-				if found {
+				// TODO(daniel) highlight the pattern
+				// TODO(daniel) make colors dynamic
+				if found != "" {
+					var col *color.Color
+					switch found {
+					case "NOTE":
+						col = color.New(color.FgHiGreen)
+					case "TODO":
+						col = color.New(color.FgHiYellow)
+					case "FIXME":
+						col = color.New(color.FgHiRed)
+					}
+
 					pos := fs.PositionFor(c.Slash, false)
-					log.Printf("%s %s", pos, c.Text)
+					if root != "" {
+						pos.Filename, _ = filepath.Rel(root, pos.Filename)
+					}
+					if first {
+						first = false
+						col.Printf("%s %s\n", pos, c.Text)
+					} else {
+						col.Printf("%*s %s\n", len(fmt.Sprintf("%s", pos)), "", c.Text)
+					}
 				}
 			}
 		}
@@ -106,6 +155,7 @@ func getCommentLines(fs *token.FileSet, cg *ast.CommentGroup) []*ast.Comment {
 			pos := file.Pos(offset)
 			offset += len(s)
 			if strings.HasPrefix(s, "//") || strings.HasPrefix(s, "/*") {
+				// TODO(daniel): Increment the c.Slash position, so we land exactly on the pattern (skip e.g. "// ")
 				s = s[2:]
 			}
 			if strings.HasSuffix(s, "*/") {
